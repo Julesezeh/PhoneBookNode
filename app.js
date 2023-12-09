054a4f1bdd53e366694c796accc671223a68e650
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-09 18:34:57 +0100

    dependency

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { v4: uuid4 } = require("uuid")
 const port = 3000;
 
 app.use(express.json());
@@ -12,7 +13,10 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     // const {phoneNumber,Name} = req.body;
-    contacts.push(req.body);
+    const id = uuid4();
+    const body = req.body;
+    const updated_body = { ...body, id: id }
+    contacts.push(updated_body);
     res.status(201).send(contacts);
 })
 
