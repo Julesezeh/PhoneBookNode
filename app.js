e33f94be08e0a6edb6f70d81cb19bdd3f728d0c3
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-10 09:15:49 +0100

    Search with Phone Number instead

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.get("/", (req, res) => {
     res.status(200).send(contacts);
 })
 
+app.get("/:id", (req, res) => {
+    const { phoneNumber } = req.params;
+    const user = contacts.find((contact) => contact.phoneNumber === phoneNumber)
+    if (user) {
+        return res.status(200).send(`<h1>${user.id}---${user.phoneNumber}----${user.fullName} </h1>`)
+    } else if (id) {
+        return res.status(404).send(`Phone number "${phoneNumber}" does not exist`)
+    }
+})
+
 app.post("/", (req, res) => {
     // const {phoneNumber,Name} = req.body;
     const id = uuid4();
@@ -19,6 +29,7 @@ app.post("/", (req, res) => {
     contacts.push(updated_body);
     res.status(201).send(contacts);
 })
+
 
 
 app.listen(port, () => {
